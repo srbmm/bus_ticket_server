90a70292871cd8135f3ac617da9b0baa46d44a6c
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-04-17 21:42:26 +0330

    branch database

--- functions/auth.js
+++ functions/auth.js
@@ -23,7 +23,6 @@ function login(table_name, username, password) {
 function signUp(table_name, data) {
     switch (table_name) {
         case TABLE.std: {
-
             break;
         }
         case TABLE.driver: {
