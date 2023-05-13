c4f842b73158aab38ad163af74f12ec217583593
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-05-13 05:35:30 +0330

    add log to sql

--- database/database.js
+++ database/database.js
@@ -28,19 +28,21 @@ class Table {
         this.tableName = tableName
     }
 
-    get(after, condition = "", choices = "*", page = 0, count = 10,  order_by = "", reverse = "ASC") {
+    get(after, condition = "", choices = "*", page = 0, count = 10, order_by = "", reverse = "ASC") {
         const connection = this.database.connection;
-        if(page){
+        if (page) {
             const tableName = this.tableName
-            connection.query(`SELECT COUNT(*) FROM ${tableName}${condition ? " WHERE " + condition : ""}`, function (err, countAll){
+            connection.query(`SELECT COUNT(*) FROM ${tableName}${condition ? " WHERE " + condition : ""}`, function (err, countAll) {
                 const SQL = `SELECT ${choices} FROM ${tableName}${condition ? " WHERE " + condition : ""} ${order_by ?" ORDER BY " + order_by + " " + reverse.toUpperCase() + " " : ""}LIMIT ${count} OFFSET ${(page-1)*count};`
+                console.log(SQL)
                 connection.query(SQL, function (err, result) {
                     after(err, result, countAll[0]['COUNT(*)'])
                 })
             })
 
-        }else{
+        } else {
             const SQL = `SELECT ${choices} FROM ${this.tableName}${condition ? " WHERE " + condition : ""}${order_by ? " ORDER BY " + order_by + " " + reverse.toUpperCase() + " " : ""};`
+            console.log(SQL)
             connection.query(SQL, function (err, result) {
                 after(err, result)
             })
@@ -69,45 +71,50 @@ class Table {
         }
         const connection = this.database.connection;
         const SQL = `INSERT INTO ${this.tableName} (${colNames}) VALUES (${values});`;
+        console.log(SQL)
         connection.query(SQL, function (err, result) {
             after(err, result)
         })
     }
+
     edit(after, data, conditions) {
-            let data_str = ""
-            let counter = 0
-            const keys = Object.keys(data)
-            for (let key of keys) {
-                if (typeof data[key] === "string" && key !== 'password') {
-                    data[key] = `'${data[key]}'`
-                }
-                if (counter === keys.length - 1) {
-                    if (key === "password") data_str += `${key} = '${md5(data[key])}'`
-                    else data_str += `${key} = ${data[key]}`
-                } else {
-                    if (key === "password") data_str += `${key} = '${md5(data[key])}',`
-                    else data_str += `${key} = ${data[key]}, `
-                }
-                counter += 1
+        let data_str = ""
+        let counter = 0
+        const keys = Object.keys(data)
+        for (let key of keys) {
+            if (typeof data[key] === "string" && key !== 'password') {
+                data[key] = `'${data[key]}'`
             }
-            const connection = this.database.connection;
-            const SQL = `UPDATE ${this.tableName} SET ${data_str} ${conditions? "WHERE " + conditions: ""};`
-            connection.query(SQL, function (err, result) {
-                after(err, result)
-            })
+            if (counter === keys.length - 1) {
+                if (key === "password") data_str += `${key} = '${md5(data[key])}'`
+                else data_str += `${key} = ${data[key]}`
+            } else {
+                if (key === "password") data_str += `${key} = '${md5(data[key])}',`
+                else data_str += `${key} = ${data[key]}, `
+            }
+            counter += 1
         }
+        const connection = this.database.connection;
+        const SQL = `UPDATE ${this.tableName} SET ${data_str} ${conditions? "WHERE " + conditions: ""};`
+        console.log(SQL)
+        connection.query(SQL, function (err, result) {
+            after(err, result)
+        })
+    }
 
     remove(after, condition) {
         if (condition) {
             const connection = this.database.connection
             const SQL = `DELETE FROM ${this.tableName} WHERE ${condition};`
+            console.log(SQL)
             connection.query(SQL, function (err, result) {
                 after(err, result)
             })
         } else
             throw new Error("please add condition")
     }
-maxBalance
+
+    maxBalance
 }
 
 module.exports = Database
